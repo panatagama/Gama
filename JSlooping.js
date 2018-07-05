73d2e8da1f4be6833995fdf3b246817d599574c9
admint <abigams23@gmail.com>
2018-07-05 13:28:53 +0700

    tugas looping

---
+++ JSlooping.js
@@ -0,0 +1,68 @@
+// soal nomor 1
+//var counter = 0;
+
+// console.log('looping pertama');
+// while (counter < 20){
+//   counter +=2
+//   console.log(counter + ' i love coding');
+  
+// }
+// console.log('looping kedua');
+// while (counter > 2){
+//   counter -=2
+//   console.log(counter + ' i will become fullstack developer');
+  
+// }
+
+//soal nomor 2
+// console.log('looping pertama');
+// for(var counter = 1 ; counter <= 20 ; counter ++) {
+//   console.log(counter + ' i love coding');
+// }
+// console.log('looping kedua');
+// for(var counter = 20 ; counter >= 1 ; counter --) {
+//   console.log(counter + ' i will become fullstack developer');
+// }
+
+//soal nomor 3
+// *1 & 2
+// for (var counter = 1; counter <= 100; counter ++){
+//   if (counter %2===0){
+//     console.log(counter + ' genap');
+//   } else {
+//     console.log(counter + ' ganjil');
+//   }
+// }
+
+//*3 & 4
+var k3 = 0;
+var k6 = 1;
+var k10 = 1;
+
+// kelipatan 3
+// while (k3 < 100){
+//   k3 += 2
+//   if (k3 %3===0){
+//     console.log(k3 + ' kelipatan 3');
+//   }else{
+//     console.log(k3 + ' ');
+//   }
+// }
+
+// while (k6 < 100){
+//   k6 += 5
+//   if (k6 %6===0){
+//     console.log(k6 + ' kelipatan 6');
+//   }else{
+//     console.log(k6 + ' ');
+//   }
+// }
+
+while (k10 < 100){
+  k10 += 9
+  if (k10 %10===0){
+    console.log(k10 + ' kelipatan 10');
+  }else{
+    console.log(k10 + ' ');
+  }
+}
