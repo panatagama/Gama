469a1ff7c6fbac7412e9884baa09ba60f5079f22
admint <abigams23@gmail.com>
2018-07-05 13:35:41 +0700

    tugas basic function

---
+++ JSfunction.js
@@ -0,0 +1,29 @@
+// tugas 1
+// function shoutOut(){
+  // console.log('Halo Function!');
+// }
+// console.log(shoutOut());
+
+// tugas 2
+// function calculateMultiply(num1, num2){
+//     return num1 * num2;  
+  
+// }
+// var num1 = 5;
+// var num2 = 6;
+// var hasilPerkalian = calculateMultiply(num1, num2);
+// console.log(hasilPerkalian);
+
+// tugas 3
+function processSentence(name,age,address,hobby){
+  return 'nama saya ' + name + ', umur saya ' + age + ', alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby;
+
+}
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence);
+// Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
