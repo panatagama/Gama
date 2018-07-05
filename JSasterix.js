36f4a23017851a8638dae4fc085110b437325a9d
admint <abigams23@gmail.com>
2018-07-05 13:28:25 +0700

    tugas asterix

---
+++ JSasterix.js
@@ -0,0 +1,20 @@
+// soal nomor 1 
+var string = ''; // <-- termasuk dlm bagian nomor 1 & 3
+for (var rows1 = 0; rows1 < 5; rows1++ ){
+  //soal nomor 1
+  // console.log('*');
+  
+  // soal nomor 2
+  // var string = '';
+  // for (var rows2 = 0; rows2 <= 5; rows2++ ){
+    // string = string + '*';
+  // } 
+  
+  // soal nomor 3
+    for (var rows3 = 0; rows3 <= 0; rows3++){
+      string = string + '*';
+      console.log(string);
+    }
+  // console.log(string);// <-- masuk bagian nomor 2
+}
+// console.log(string);
